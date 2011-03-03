7cbf833a4d89c6ee5706035c658125171d704f1b
carloleonardi83 <carloleonardi83@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-03 16:05:42 +0000

    Tante cose

--- trunk/demo-ontology/index.php
+++ trunk/demo-ontology/index.php
@@ -15,7 +15,7 @@ Released   : 20081210
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Long Beach by Free CSS Templates</title>
+<title>Demography Ontology</title>
 <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
@@ -49,14 +49,16 @@ Released   : 20081210
 	<!-- end #menu -->
 	<div id="page">
 		<div id="content">
-			<div class="post">
-				<h1 class="title"><a href="#">Welcome to Long Beach </a></h1>
-				<p class="meta">Posted by <a href="#">Someone</a> on March 10, 2008
+			<div class="post" >
+				<h1 class="title"><a href="#">Demography Ontology </a></h1>
+                                
+                                <p class="meta" >Posted by <a href="#">Someone</a> on March 10, 2008
 					&nbsp;&bull;&nbsp; <a href="#" class="comments">Comments (64)</a> &nbsp;&bull;&nbsp; <a href="#" class="permalink">Full article</a></p>
 				<div class="entry">
 					<p>This is <strong>Long Beach</strong>, a free, fully standards-compliant CSS template designed byFreeCssTemplates<a href="http://www.nodethirtythree.com/"></a> for <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>. This free template is released under a <a href="http://creativecommons.org/licenses/by/2.5/">Creative Commons Attributions 2.5</a> license, so you’re pretty much free to do whatever you want with it (even use it commercially) provided you keep the links in the footer intact. Aside from that, have fun with it :)</p>
 					<p>Sed lacus. Donec lectus. Nullam pretium nibh ut turpis. Nam bibendum. In nulla tortor, elementum ipsum. Proin imperdiet est. Phasellus dapibus semper urna. Pellentesque ornare, orci in felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem.</p>
-				</div>
+                                        <p id='titolo'></p>
+                                </div>
 			</div>
 			<div class="post">
 				<h2 class="title"><a href="#">Lorem ipsum sed aliquam</a></h2>
@@ -80,6 +82,105 @@ Released   : 20081210
 		<div id="sidebar">
 			<ul>
 				<li>
+                                
+                                          <?php
+//                                            include_once ( 'HTTP/Request.php' );
+//                                            $sesame_url = "http://localhost:8080/openrdf-sesame";
+
+                                            /*
+                                            SELECT ?z WHERE { ?x ?y ?z. }
+                                            */
+                                            //$query = "?query=select%20?z%20WHERE%20%7B%20?x%20?y%20?z.%20%7D";
+
+                                            /*
+                                            $query = "?query=select%20?z%20WHERE%20{%20?x%20?y%20?z.}";
+                                            $requestString = $sesame_url.'/repositories/12'.$query;
+                                            */
+                                            //$query = "?query=PREFIX%20Ontology12989921859012:<http://www.semanticweb.org/ontologies/2011/2/Ontology1298992185901.owl#>%20PREFIX%20rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>%20SELECT%20?provincia%20WHERE{?provincia%20rdf:type%20Ontology12989921859012:Province.}";
+                                            //$query = "?query=select%20?x%20WHERE%20{%20?x%20?y%20?<http://www.semanticweb.org/ontologies/2011/2/Ontology1298992185901.owl#>Province.}";
+                                            //$query = "?query=%0APREFIX%20Ontology12989921859012:<http://www.semanticweb.org/ontologies/2011/2/Ontology1298992185901.owl%23>%0ASELECT%20%3Fprovincia%0AWHERE{%0A%3Fprovincia%20%3Fx%20Ontology12989921859012:Province.}";
+                                            //$query = "?queryLn=SPARQL&query=PREFIX%20rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns%23>%0APREFIX%20Ontology12989921859012:<http://www.semanticweb.org/ontologies/2011/2/Ontology1298992185901.owl%23>%0ASELECT%20%3Fprovincia%0AWHERE{%3Fx%20rdf:type%20Ontology12989921859012:Province.%3Fx%20Ontology12989921859012:nameIs%20%3Fprovincia.}";
+//                                            $query ='?queryLn=SPARQL&query=PREFIX%20Ontology12989921859012:%3Chttp://www.semanticweb.org/ontologies/2011/2/Ontology1298992185901.owl%23%3E%0APREFIX%20xsd:%3Chttp://www.w3.org/2001/XMLSchema%23%3E%0ASELECT%20%3Fy%0AWHERE{%0A%3Fx%20Ontology12989921859012:nameIs%20%22Catania%22^^xsd:string.%0A%3Fx%20Ontology12989921859012:hasMunicipality%20%3Fy.%0A}&limit=100&infer=true';
+//                                            $requestString = $sesame_url.'/repositories/demography'.$query;
+//                                            $req =& new HTTP_Request($requestString);
+//                                            //echo $requestString;
+//                                            $req->setMethod(HTTP_REQUEST_METHOD_GET);
+//                                            $req->addHeader("Accept", "application/sparql-results+xml, */*;q=0.5");
+//                                            $req->sendRequest();
+//                                            $response_code = $req->getResponseCode();
+//                                            if($response_code!=200)
+//                                                    echo "Errore di codice ".$response_code;
+//                                            else
+//                                                {
+//                                                $response_body = $req->getResponseBody();
+                                                //echo "Risposta ricevuta correttamente<br/><br>";
+//                                                echo $response_body."<br/><br/>";
+//                                                $xml=simplexml_load_string($response_body);
+//                                                $address = new SimpleXMLElement($response_body);
+                                                //foreach($xml->results->result as $item){
+                                                //echo $item->binding->literal;
+                                                //}
+                                               //}
+
+                                //            GET /openrdf-sesame/repositories/mem-rdf?query=select%20%3Cfoo:bar%3E&queryLn=serql HTTP/1.1
+                                //            Host: localhost
+                                //            Accept: application/sparql-results+xml, */*;q=0.5
+                                        ?>
+                                         
+
+
+<form name="frm" method="post"> <!--action='submit.php'>-->
+
+<pre>
+Year:           <select name="year" id="year">
+                    <option value="0">-- Selection --</option>
+              </select>
+Province:       <select name="prov" onChange="loadTowns(this.value)">
+               <option value="0">-- Selection --</option>
+                     <?php
+                        include_once ( 'HTTP/Request.php' );
+                        $sesame_url = "http://localhost:8080/openrdf-sesame";
+                        $query = "?queryLn=SPARQL&query=PREFIX%20rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns%23>%0APREFIX%20DemoOntology:<http://demo-ontology.googlecode.com/svn/trunk/demo-ontology/DemoOntology.owl%23>%0Aselect%20%3Fy%0Awhere{%0A%3Fx%20rdf:type%20DemoOntology:Province.%0A%3Fx%20DemoOntology:hasName%20%3Fy}";
+                        $requestString = $sesame_url.'/repositories/demography'.$query;
+                        $req =& new HTTP_Request($requestString);
+                        //echo $requestString;
+                        $req->setMethod(HTTP_REQUEST_METHOD_GET);
+                        $req->addHeader("Accept", "application/sparql-results+xml, */*;q=0.5");
+                        $req->sendRequest();
+                        $response_code = $req->getResponseCode();
+                        if($response_code!=200)
+                                echo "Errore di codice ".$response_code;
+                        else
+                            {
+                            $response_body = $req->getResponseBody();
+                            //echo "Risposta ricevuta correttamente<br/><br>";
+                            //echo $response_body."<br/><br/>";
+                            $xml=simplexml_load_string($response_body);
+                            $address = new SimpleXMLElement($response_body);
+                            foreach($xml->results->result as $item){
+                            $value=$item->binding->literal;
+                            echo '<option value="'.$value.'">'.$value.'</option>';
+                            }
+                           }
+                                              
+                       ?>                    
+            </select>
+Municipality:   <select name="town" id="town">
+                    <option value="0">-- Selection --</option>
+              </select>
+Sex:            <select name="sex" id="sex">
+                    <option value="0">-- Selection --</option>
+                    <option value="All">All</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+              </select>
+Marital Status:
+<input  type="checkbox" name="Unmarried" id="Unmarried">Unmarried</input>    <input  type="checkbox" name="Married" id="Married">Married</input>
+<input  type="checkbox" name="Divorced" id="Divorced">Divorced</input>     <input  type="checkbox" name="Widowed" id="Widowed">Widowed</input>
+</pre>
+</form>
+<button onClick="prova()"> Invia </button>
+                                        
 					<h2>Aliquam tempus</h2>
 					<p>Mauris vitae nisl nec metus placerat perdiet est. Phasellus dapibus semper urna ornare, orci in consectetuer hendrerit.</p>
 				</li>
@@ -124,4 +225,66 @@ Released   : 20081210
 <!-- end #footer -->
 </div>
 </body>
+<script language="javascript">
+var xmlHttp
+
+function loadTowns(str) {
+   xmlHttp=GetXmlHttpObject()
+   if (xmlHttp==null) {
+      alert ("Browser does not support HTTP Request")
+      return
+   }
+   var url="townloader.php"
+
+   url=url+"?prov="+str
+   url=url+"&sid="+Math.random()
+   xmlHttp.onreadystatechange=stateChanged
+   xmlHttp.open("GET",url,true)
+   xmlHttp.send(null)
+}
+
+function stateChanged() {
+   if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete") {
+      document.getElementById("town").innerHTML=xmlHttp.responseText
+   }
+}
+function divChanged() {
+   if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete") {
+      document.getElementById("titolo").innerHTML=xmlHttp.responseText
+   }
+}
+function GetXmlHttpObject() {
+   var xmlHttp=null;
+   try {
+      // Firefox, Opera 8.0+, Safari
+      xmlHttp=new XMLHttpRequest();
+   } catch (e) {
+      //Internet Explorer
+      try {
+         xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+      } catch (e) {
+         xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+   }
+   return xmlHttp;
+}
+function prova(){
+    var s=document.forms['frm'].elements['town'].options[document.forms['frm'].elements['town'].options.selectedIndex].value;
+    //alert(s);
+    document.getElementById('titolo').innerHTML=s;
+    xmlHttp=GetXmlHttpObject()
+   if (xmlHttp==null) {
+      alert ("Browser does not support HTTP Request")
+      return
+   }
+   var url="structquery.php"
+
+   url=url+"?s="+s;
+   //url=url+"&sid="+Math.random()
+   xmlHttp.onreadystatechange=divChanged
+   xmlHttp.open("GET",url,true)
+   xmlHttp.send(null)
+}
+</script>
 </html>
+
---
+++ trunk/demo-ontology/structquery.php
@@ -0,0 +1,10 @@
+<?php
+$s=$_GET["s"];
+echo $s;
+echo '<div>'.$s."pippo".'</div>'
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
