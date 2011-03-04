65795c0665b979aa19b9f3c5d96b8631687a2286
carloleonardi83 <carloleonardi83@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-04 11:22:44 +0000

    Tante cose

--- trunk/demo-ontology/structquery.php
+++ trunk/demo-ontology/structquery.php
@@ -1,7 +1,13 @@
 <?php
 $s=$_GET["s"];
-echo $s;
-echo '<div>'.$s."pippo".'</div>'
+$s2=$_GET["s2"];
+$Married=$_GET["Married"];
+$Unmarried=$_GET["Unmarried"];
+$Divorced=$_GET["Divorced"];
+$Widowed=$_GET["Widowed"];
+//echo $s;
+echo '<div>'.$s.'  '.$s2.' Married:'.$Married.' Unmarried'.$Unmarried.' Divorced'.$Divorced.' Widowed '.$Widowed.'</div>'
+
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
