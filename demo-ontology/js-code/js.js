d874a688aa5f0cb23747bfdf27b8ba6ff8053675
carloleonardi83 <carloleonardi83@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-04 11:23:42 +0000

    Tante cose

--- demo-ontology/js-code/js.js
+++ demo-ontology/js-code/js.js
@@ -41,9 +41,16 @@ function GetXmlHttpObject() {
    return xmlHttp;
 }
 function prova(){
-    var s=document.forms['frm'].elements['town'].options[document.forms['frm'].elements['town'].options.selectedIndex].value;
-    //alert(s);
-    document.getElementById('titolo').innerHTML=s;
+  var s=document.forms['frm'].elements['town'].options[document.forms['frm'].elements['town'].options.selectedIndex].value;
+    var s2=document.forms['frm'].elements['sex'].options[document.forms['frm'].elements['sex'].options.selectedIndex].value;
+    var Unmarried=document.forms['frm'].elements['Unmarried'].value;
+    var Married=document.forms['frm'].elements['Married'].value;
+    var Widowed=document.forms['frm'].elements['Widowed'].value;
+    var Divorced=document.forms['frm'].elements['Divorced'].value;
+
+      //alert(s2);
+    //document.getElementById('titolo').innerHTML=s;
+     //document.getElementById('titolo').innerHTML=s2;
     xmlHttp=GetXmlHttpObject()
    if (xmlHttp==null) {
       alert ("Browser does not support HTTP Request")
@@ -51,7 +58,7 @@ function prova(){
    }
    var url="structquery.php"
 
-   url=url+"?s="+s;
+   url=url+"?s="+s+"&s2="+s2+"&Unmarried="+Unmarried+"&Married="+Married+"&Widowed="+Widowed+"&Divorced="+Divorced;
    //url=url+"&sid="+Math.random()
    xmlHttp.onreadystatechange=divChanged
    xmlHttp.open("GET",url,true)
