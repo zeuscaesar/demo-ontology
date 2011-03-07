3d5756b767dfb46f05d8338db76298efc18f9aa4
luciano.defranco <luciano.defranco@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-07 13:48:47 +0000

    DemoOntology.owl

--- trunk/demo-ontology/js-code/js-graph.js
+++ trunk/demo-ontology/js-code/js-graph.js
@@ -1,6 +1,17 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+function grafico(data)
+{
+    for(i = 0; i < data.length; i++)
+    {
+        //document.write(data.length);
+        //document.write("<span class='G' style='width: " + data[i] + "px;'>");
+        //document.write("&nbsp;");
+        //document.write("</span>");
+        var h=data[i];
+        var w=2.5;
+        document.write("<img src='blank.gif' alt='"+h+"' class='barra2' style='height: " + h + "px; width: " + w + "px;'></img>");
 
 
+        
+        //document.write("<span class='barra' style='height: " + data[i] + "px;'>");
+    }
+}
