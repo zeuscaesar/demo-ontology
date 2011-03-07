06e983f356b5f4d56ad277f8474e1afa35510ebe
luciano.defranco <luciano.defranco@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-07 17:10:39 +0000

    DemoOntology.owl

--- trunk/demo-ontology/js-code/js-graph.js
+++ trunk/demo-ontology/js-code/js-graph.js
@@ -2,16 +2,13 @@ function grafico(data)
 {
     for(i = 0; i < data.length; i++)
     {
-        //document.write(data.length);
-        //document.write("<span class='G' style='width: " + data[i] + "px;'>");
-        //document.write("&nbsp;");
-        //document.write("</span>");
+     
         var h=data[i];
-        var w=2.9;
+        var w=3;
         document.write("<img src='blank.gif' alt='"+i+" anni -> "+h+" abitanti' title='"+i+" anni -> "+h+" abitanti' class='barra2' style='height: " + h + "px; width: " + w + "px;'/>");
 
 
         
-        //document.write("<span class='barra' style='height: " + data[i] + "px;'>");
+
     }
 }
