38b30362dbda20c7ddebdc8d459ff2ab3460f9ab
carloleonardi83 <carloleonardi83@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-10 14:43:40 +0000

    Tante cose

--- demo-ontology/js-code/js.js
+++ demo-ontology/js-code/js.js
@@ -48,13 +48,14 @@ function divChanged() {
         if((q-max)>0){max=q;}
     }
     var norm=indice/max;
-     for(i = 0; i < data.length; i++)
+     for(i = 0; i < 100; i++)
     {
         var h=data[i];
         //if(max<h){max=h;}
         var w=3;
         output=output+"<img src='blank.gif' alt='"+i+" anni -> "+h+" abitanti' title='"+i+" anni -> "+h+" abitanti ->max="+max+"' class='barra2' style='height: " + (norm*h) + "px; width: " + w + "px;'/>";
     }
+
       document.getElementById("divdata").innerHTML=output;
    }
 }
@@ -89,9 +90,10 @@ function prova(){
     var Divorced=document.forms['frm'].elements['Divorced'].value;
 
       //alert(s2);
+     var table= "<table border='0.5'><tr><td>prima cella</td><td>seconda cella</td></tr><tr><td>terza cella</td><td>quarta cella</td></tr></table>";
     //document.getElementById('titolo').innerHTML=s;
     //document.getElementById('divdata').innerHTML="pippo";
-    document.getElementById('pdiv').innerHTML="pippo";
+    document.getElementById('pdiv').innerHTML=table;
     document.getElementById('number').innerHTML="0      10      20      30      40      50      60      70      80      90      100";
 
     xmlHttp=GetXmlHttpObject()
