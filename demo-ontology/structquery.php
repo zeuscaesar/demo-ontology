3d6f9e38a62c6208c409350f9e1ba1942941ba51
alexgpeppe <alexgpeppe@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-14 08:57:29 +0000

    Aggiunta dei commenti sull'ontologia Aggiunto calcolo e grafico della popolazione totale

--- demo-ontology/structquery.php
+++ demo-ontology/structquery.php
@@ -144,18 +144,26 @@ $requestString = $sesame_url.'/repositories/demography'.$query;
                 $xml=simplexml_load_string($response_body);
                 //echo $response_body;
                 //$address = new SimpleXMLElement($response_body);
-                foreach($xml->results->result as $item){
-                $value=$item->binding->literal;
-                //echo '<div>'.$value.'</div>';
-                echo $value;
-//                echo "<script type=\"text/javascript\">\n";
-//                echo "var data=new Array(105,89,83,92,93,85,75,78,65,75,81,86,80,74,95,82,81,107,96,103,114,94,108,100,93,91,94,103,63,49,77,48,53,52,39,52,41,39,38,33,32,27,31,30,24,20,18,18,16,21,12,14,17,17,12,12,12,12,13,8,8,10,8,5,9,13,7,7,5,10,7,6,8,6,8,5,11,6,7,5,7,5,3,6,2,7,4,5,3,3,1,0,1,0,1,0,0,0,1,0,10);\n";
-//                echo "grafico(data);\n";
-//                echo "alert(\"accura\");\n";
-//                echo "</script>\n";
-               
-                    }
+                //INIZIO VERSIONE FUNZIONANTE
+//                foreach($xml->results->result as $item){
+//                    $value=$item->binding->literal;
+//                    echo $value."|";
+//                    }
+                //FINE VERSIONE FUNZIONANTE
+
+                // Creo un array contenente i nomi delle variabili restituite dalla query
+                $arr = array();
+                foreach($xml->head->variable->attributes as $name => $val)
+                    //appendo all'array il nome letto
+                    $arr[] = $val;
                 }
+                foreach($xml->results->result as $item){
+                    foreach ($item->binding as $ $val) {
+
+                    }
+                    $value = $item->binding;
+                    echo $value."|";
+                    }
 //echo '<div>'.$year.'  '.$prov.' '.$town.' '.$sex.' Married:'.$Married.' Unmarried:'.$Unmarried.' Divorced:'.$Divorced.' Widowed:'.$Widowed.'</div>'
 
 /* 
