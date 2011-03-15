6b2d69f86be8876b7d1838b0986de02eab2aa6fb
luciano.defranco <luciano.defranco@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-15 12:54:04 +0000

    modifica .html in .php

---
+++ trunk/demo-ontology/about.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="OWL,Demography,Ontology" />
+<meta name="description" content="This is Demography Ontology, a project developed at the Computer Science and Telecomunication department of the University of Catania." />
+<meta name="author" content="Luciano De Franco, Giuseppe Alessandro, Carlo Leonardi"/>
+<meta name="copyright" content="Luciano De Franco, Giuseppe Alessandro, Carlo Leonardi"/>
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Demography Ontology - Home Page</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="wrapper">
+	<div id="header">
+		<div id="logo">
+			<h1><a href="#">Demography Ontology</a></h1>
+			<p><a href=""></a></p>
+		</div>
+		<div id="search">
+			<form method="get" action="">
+				<fieldset>
+				<input id="search-text" type="text" name="s" value="Search" size="15" />
+				<input type="submit" id="search-submit" value="Search" />
+				</fieldset>
+			</form>
+		</div>
+		<!-- end #search -->
+	</div>
+	<!-- end #header -->
+	<div id="menu">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="search.php">Simple Search</a></li>
+			<li><a href="geosearch.php">Geo Search</a></li>
+                        <li><a href="growth.php">Growth</a></li>
+			<li><a href="freequery.php">Free Query</a></li>
+			<li><a href="help.php">Help</a></li>
+			<li><a href="about.php">About</a></li>
+		</ul>
+	</div>
+	<!-- end #menu -->
+	<div id="page">
+		<div id="content">
+			
+			<div class="post">
+				<h2 class="title"><a href="#">Giuseppe Alessandro</a></h2>
+				<p class="meta">Updated on March 15, 2011</p>
+				<div class="entry">
+				
+					<p>Praesent ac lectus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus augue. Fusce eget tellus ultrices ligula volutpat adipiscing. Aenean ligula lectus, vehicula in, dictum a, fermentum nec, felis. Nunc ac turpis in leo posuere imperdiet.</p>
+
+                        <p>
+                            <strong>Web-site: </strong><a href="#"></a>
+                            <br/>
+                            <strong>E-mail: </strong><a href="mailto:alexgpeppe84@hotmail.it">alexgpeppe@gmail.com</a>
+                        </p>
+                        
+                        <p>
+                            <strong>MSN: </strong><a href="msnim:add?contact=alexgpeppe84@hotmail.it">alexgpeppe84@hotmail.it</a>
+                            <br/>
+                            <strong>Skype: </strong><a href="skype:alexgpeppe?call">alexgpeppe</a>
+                        </p>
+
+                        <p>
+                        	<strong>Facebook: </strong><a href="http://www.facebook.com/alexgpeppe">http://www.facebook.com/alexgpeppe</a>
+                                <br/>
+                                <strong>LinkedIN: </strong><a href="http://it.linkedin.com/pub/giuseppe-alessandro/2b/966/31b">http://it.linkedin.com/pub/giuseppe-alessandro/2b/966/31b</a>
+                                <br/>
+                        </p>
+
+				</div>
+			</div>
+			<div class="post">
+				<h2 class="title"><a href="#">Luciano De Franco</a></h2>
+				<p class="meta">Updated on March 15, 2011</p>
+				<div class="entry">
+					
+					<p>Praesent ac lectus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus augue. Fusce eget tellus ultrices ligula volutpat adipiscing. Aenean ligula lectus, vehicula in, dictum a, fermentum nec, felis. Nunc ac turpis in leo posuere imperdiet.</p>
+                                        <p>
+                            <strong>Web-site: </strong><a href="http://lucianodefranco.altervista.org/">luciano.defranco@gmail.com</a>
+                            <br/>
+                            <strong>E-mail: </strong><a href="mailto:luciano.defranco@gmail.com">luciano.defranco@gmail.com</a>
+                        </p>
+
+                        <p>
+                            <strong>MSN: </strong><a href="msnim:add?contact=luxhacker@hotmail.com">luxhacker@hotmail.com</a>
+                            <br/>
+                            <strong>Skype: </strong><a href="skype:luxhacker?call">luxhacker</a>
+                        </p>
+
+                        <p>
+                        	<strong>Facebook: </strong><a href="http://www.facebook.com/lucianodefranco">http://www.facebook.com/lucianodefranco</a>
+                                <br/>
+                                <strong>LinkedIN: </strong><a href="http://it.linkedin.com/pub/luciano-de-franco/18/99b/599">http://it.linkedin.com/pub/luciano-de-franco/18/99b/599</a>
+                                <br/>
+                        </p>
+				</div>
+                        </div>
+                        <div class="post">
+				<h2 class="title"><a href="#">Carlo Leonardi</a></h2>
+				<p class="meta">Updated on March 15, 2011</p>
+				<div class="entry">
+					
+					<p>Praesent ac lectus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus augue. Fusce eget tellus ultrices ligula volutpat adipiscing. Aenean ligula lectus, vehicula in, dictum a, fermentum nec, felis. Nunc ac turpis in leo posuere imperdiet.</p>
+                                        <p>
+                            <strong>Web-site: </strong><a href="#"></a>
+                            <br/>
+                            <strong>E-mail: </strong><a href="mailto:carloleonardi83@gmail.com">carloleonardi83@gmail.com</a>
+                        </p>
+
+                        <p>
+                            <strong>MSN: </strong><a href="msnim:add?contact=carloleonardi83@hotmail.it">carloleonardi83@hotmail.it</a>
+                            <br/>
+                            <strong>Skype: </strong><a href="#"></a>
+                        </p>
+
+                        <p>
+                        	<strong>Facebook: </strong><a href="http://www.facebook.com/carlo.leonardi">http://www.facebook.com/carlo.leonardi</a>
+                                <br/>
+                                <strong>LinkedIN: </strong><a href="#"></a>
+                                <br/>
+                        </p>
+				</div>
+                        </div>
+                </div>
+		<!-- end #content -->
+		<div id="sidebar">
+			<ul>
+				
+				<li>
+					<h2>Developers</h2>
+					<ul>
+						<li><a href="#">Giueseppe Alessandro</a> </li>
+						<li><a href="#">Luciano De Franco</a> </li>
+						<li><a href="#">Carlo Leonardi</a> </li>
+						
+					</ul>
+				</li>
+				<li>
+					<h2>Other projects</h2>
+					<ul>
+						<li><a href="http://code.google.com/p/sd2010/">SD2010</a></li>
+						<li><a href="http://code.google.com/p/ccn-ct/">CCN-CT</a></li>
+						<li><a href="http://code.google.com/p/socialsingleshot/">S3: Social-Single-Shot</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+		</div>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<!-- end #page -->
+<div id="footer">
+	<p>Copyright (c) 2011 DemographyOntology.com. All rights reserved. Design by <a href="mailto:alexgpeppe84@hotmail.it">GA</a> - <a href="http://lucianodefranco.altervista.org/">LDF</a> - <a href="mailto:carloleonardi83@gmail.com">CL</a>.</p>
+</div>
+<!-- end #footer -->
+</div>
+</body>
+</html>
