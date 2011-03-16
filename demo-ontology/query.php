3c1f5dc751932758feb26c03f4eb7477529a2bb1
carloleonardi83 <carloleonardi83@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-16 09:24:41 +0000

    Tante cose

--- demo-ontology/query.php
+++ demo-ontology/query.php
@@ -25,5 +25,22 @@ function concatRDF($query,$concat){
 function closeRDF($query){
     return $query=$query.'}';
 }
+function writeRDF($query){
+    $pre="?queryLn=SPARQL&query=";
+    $prefix='
+PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>
+PREFIX owl:<http://www.w3.org/2002/07/owl#>
+PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX DemoOntology:<http://demo-ontology.googlecode.com/svn/trunk/demo-ontology/DemoOntology.owl#>';
+    $prefix=str_replace(" ", "%20", $prefix);
+    $prefix=str_replace("\n","%0A", $prefix);
+    $prefix=str_replace("#", "%23", $prefix);
+    $query=str_replace("#", "%23", $query);
+    $query=str_replace(" ", "%20", $query);
+    $query=str_replace("?", "%3F", $query);
+    $query=str_replace("\n","%0A", $query);
+    return "?".$pre.$prefix.$query;
+}
 ?>
 
---
+++ demo-ontology/queryloader.php
@@ -0,0 +1,35 @@
+<?php
+$area=$_GET["area"];
+
+include_once ( 'HTTP/Request.php' );
+include_once ('query.php');
+                $sesame_url = "http://localhost:8080/openrdf-sesame";
+                echo $area;
+                $query=writeRDF($area);
+                $requestString = $sesame_url.'/repositories/demography'.$query;
+                $req =& new HTTP_Request($requestString);
+                //echo $requestString;
+                $req->setMethod(HTTP_REQUEST_METHOD_GET);
+                $req->addHeader("Accept", "application/sparql-results+xml, */*;q=0.5");
+                $req->sendRequest();
+                $response_code = $req->getResponseCode();
+                if($response_code!=200)
+                echo "Errore di codice ".$response_code;
+                else
+                {
+                $response_body = $req->getResponseBody();
+                //echo "Risposta ricevuta correttamente<br/><br>";
+                //echo $response_body."<br/><br/>";
+                //echo "Risposta ricevuta correttamente<br/><br>";
+                //echo $response_body."<br/><br/>";
+                $xml=simplexml_load_string($response_body);
+                echo '<div>scrivo qui</div>';
+                echo '<div>'.$response_body.'</div>';
+                //$address = new SimpleXMLElement($response_body);
+                foreach($xml->results->result as $item){
+                $value=$item->binding->literal;
+                echo '<p>'.$value.'</p>';
+                }
+}
+
+?> 
