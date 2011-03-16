72aecdae6589757eb99566e68f2cb59a936ffc64
carloleonardi83 <carloleonardi83@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-16 16:35:50 +0000

    Tante cose

---
+++ trunk/demo-ontology/yearloader.php
@@ -0,0 +1,37 @@
+<?php
+$year=$_GET["year"];
+echo $year;
+echo "<option value='0'>--All Province--</option>\n";
+include_once ( 'HTTP/Request.php' );
+include_once ('query.php');
+                $sesame_url = "http://localhost:8080/openrdf-sesame";
+      
+                $query=openRDF('select distinct ?x where{?y DemoOntology:livingInTheYear ?x.');
+                $filter='(?x>='.$year.')';
+                $query=closeRDF($query);
+                echo $query;
+                $requestString = $sesame_url.'/repositories/demography'.$query;
+                $req =& new HTTP_Request($requestString);
+                //echo $requestString;
+                $req->setMethod(HTTP_REQUEST_METHOD_GET);
+                $req->addHeader("Accept", "application/sparql-results+xml, */*;q=0.5");
+                $req->sendRequest();
+                $response_code = $req->getResponseCode();
+                if($response_code!=200){echo "Errore di codice ".$response_code;}
+                else
+                {
+                $response_body = $req->getResponseBody();
+                //echo "Risposta ricevuta correttamente<br/><br>";
+                echo $response_body."<br/><br/>";
+                //echo "Risposta ricevuta correttamente<br/><br>";
+                //echo $response_body."<br/><br/>";
+                $xml=simplexml_load_string($response_body);
+                echo $response_body;
+                //$address = new SimpleXMLElement($response_body);
+                foreach($xml->results->result as $item)
+                    {
+                    $value=$item->binding->literal;
+                    echo '<option value="'.$value.'">'.$value.'</option>';
+                    }
+                }
+?> 
