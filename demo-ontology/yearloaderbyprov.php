0d1995f7ecb21ddfd7b7a1696a97c611123bd1dd
carloleonardi83 <carloleonardi83@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-24 11:09:49 +0000

    Tante cose

---
+++ demo-ontology/yearloaderbyprov.php
@@ -0,0 +1,38 @@
+<?php
+$prov=$_GET["prov"];
+echo $year;
+include_once ( 'HTTP/Request.php' );
+include_once ('query.php');
+                $sesame_url = "http://localhost:8080/openrdf-sesame";
+                //FILTER (x? >='.$year.'
+                $query='select distinct ?x where{?y rdf:type DemoOntology:Province. ?y DemoOntology:hasName "'.$mun.'"^^rdfs:Literal. ?y DemoOntology:hasPopulation ?pop. ?pop DemoOntology:livingInTheYear ?x.}ORDER by ?x';
+                $query=openRDF($query);
+                //$query=closeRDF($query);
+                echo $query;
+                $requestString = $sesame_url.'/repositories/demography'.$query;
+                $req =& new HTTP_Request($requestString);
+                //echo $requestString;
+                $req->setMethod(HTTP_REQUEST_METHOD_GET);
+                $req->addHeader("Accept", "application/sparql-results+xml, */*;q=0.5");
+                $req->sendRequest();
+                $response_code = $req->getResponseCode();
+                if($response_code!=200){echo "Errore di codice ".$response_code;}
+                else
+                {
+                $response_body = $req->getResponseBody();
+                //echo "Risposta ricevuta correttamente<br/><br>";
+                echo $response_body."<br/><br/>";
+                //echo "Risposta ricevuta correttamente<br/><br>";
+                //echo $response_body."<br/><br/>";
+                $xml=simplexml_load_string($response_body);
+                echo $response_body;
+                
+                //$address = new SimpleXMLElement($response_body);
+                foreach($xml->results->result as $item)
+                    {
+                    $value=$item->binding->literal;
+
+                    echo '<option value="'.$value.'">'.$value.'</option>';
+                    }
+                }
+?> 
