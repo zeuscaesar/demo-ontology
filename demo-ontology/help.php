c0046b9fb2ebbb2c93f00a17e8d3ea9b9f4eaa05
alexgpeppe <alexgpeppe@4f17642b-f64e-15de-252c-c5bd4a99e6ef>
2011-03-28 10:38:01 +0000

    Help definitivo

--- demo-ontology/help.php
+++ demo-ontology/help.php
@@ -50,7 +50,7 @@
                                     <p class="meta"></p>
 				
 					<p>This page allows you to query the SESAME repository to get information about population living in a given year in a given geographical area (a specific municipality, or a province, or the whole Italian territory), according to the selected sex and marital status.</p>
-                                        <p>After having filled the form on the left side of the page, by clicking the \"Start Query\" button a graphic and a table appear: they show the result of user's query. The graphic shows the total amount of people which forms the population partitioned by age. The table shows the amount of people which forms the population partitioned by age, sex and marital status.</p>
+                                        <p>After having filled the form on the left side of the page, by clicking the "Start Query" button a graphic and a table appear: they show the result of user's query. The graphic shows the total amount of people which forms the population partitioned by age. The table shows the amount of people which forms the population partitioned by age, sex and marital status.</p>
 				
 				
 			</div>
